734eec80b7c43d016808fe6586b479d688b16f34
Nick Zahornyi <nick_surveyor@ukr.net>
2016-01-15 13:37:37 +0200

    added js hw 1-2

---
+++ GoIT/JS/lesson1-2/part1/js/script.js
@@ -0,0 +1,17 @@
+var x = prompt('Введите число, которое будем возводить в степень');
+var n = prompt('Введите степень, в которую будем возводить число');
+
+
+function pow(x, n) {
+  var result = 1;
+
+  for (var i = 0; i < n; i++) {
+	result = result * x;
+  }
+  
+  return result;
+}
+
+var powResult = pow(x, n);
+
+console.log(powResult);
