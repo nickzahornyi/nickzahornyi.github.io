7230aaf31cd0a332b114b03e5c9a3bfa39ac97fd
Nick Zahornyi <nick_surveyor@ukr.net>
2016-01-23 21:22:46 +0200

    added changes

--- GoIT/JS/lesson1-2/part1/js/script.js
+++ GoIT/JS/lesson1-2/part1/js/script.js
@@ -1,17 +1,22 @@
-var x = prompt('Введите число, которое будем возводить в степень');
-var n = prompt('Введите степень, в которую будем возводить число');
-
+var x = +prompt('Введите число, которое будем возводить в степень');
+var n = +prompt('Введите степень, в которую будем возводить число');
 
 function pow(x, n) {
   var result = 1;
+  var abs = Math.abs(n);
 
-  for (var i = 0; i < n; i++) {
-	result = result * x;
-  }
-  
-  return result;
+for (var i = 0; i < abs; i++) {
+	result = result * x; 
+}
+if (n > 0) {
+ 	return result;
+} else {
+	result = 1 / result;
+    return result;
+}
 }
 
 var powResult = pow(x, n);
 
 console.log(powResult);
+alert(powResult)
