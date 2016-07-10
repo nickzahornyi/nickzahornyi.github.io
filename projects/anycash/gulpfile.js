af27e24988fbb795cd81d8911c9f77b286db3940
Nick Zahornyi <nick_surveyor@ukr.net>
2016-07-10 23:51:23 +0300

    added some changes

---
+++ projects/anycash/gulpfile.js
@@ -0,0 +1,76 @@
+var gulp       = require('gulp'),
+	sass         = require('gulp-sass'),
+	browserSync  = require('browser-sync'),
+	cssnano      = require('gulp-cssnano'),
+	rename       = require('gulp-rename'),
+	del          = require('del'),
+	imagemin     = require('gulp-imagemin'),
+	pngquant     = require('imagemin-pngquant'),
+	cache        = require('gulp-cache'),
+	autoprefixer = require('gulp-autoprefixer'),
+	cleanCSS 		 = require('gulp-clean-css');
+gulp.task('scss', function(){
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
+		},
+		notify: false
+	});
+});
+
+gulp.task('css-libs', ['scss'], function() {
+	return gulp.src('src/css/libs.css')
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(cssnano())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', ['browser-sync', 'css-libs'], function() {
+	gulp.watch('src/scss/**/*.scss', ['scss']);
+	gulp.watch('src/*.html', browserSync.reload);
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+gulp.task('img', function() {
+	return gulp.src('src/img/**/*')
+		.pipe(cache(imagemin({
+			interlaced: true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['clean', 'img', 'scss'], function() {
+
+	var buildCss = gulp.src([
+		'src/css/main.css',
+		])
+	.pipe(gulp.dest('dist/css'))
+
+	var buildFonts = gulp.src('src/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'))
+
+	var buildHtml = gulp.src('src/*.html')
+	.pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function (callback) {
+	return cache.clearAll();
+})
+
+gulp.task('default', ['watch']);
